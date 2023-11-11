bbd91180ee79a2365b78cb52e62bb74399017c28
Zzrk <1252124617@qq.com>
2023-11-11 23:25:41 +0800

    feat: send_message + readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+## setup
+create an .env file at the root directory
+```
+BASE_URL=https://open.feishu.cn/open-apis
+APP_ID=xxxxxxxxxxxxxxxxxxxx
+APP_SECRET=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
--- src/routes/chatgpt.rs
+++ src/routes/chatgpt.rs
@@ -1,7 +1,11 @@
+use crate::models::{
+    feishu_api::{FeishuApi, ReceiveIdType, SendMessageBody},
+    feishu_message::MessageReceiveRequest,
+};
 use rocket::serde::json::{json, Json, Value};
+use std::env;
 
 // use crate::models::feishu_connect::{ConnectRequest, ConnectResponse};
-use crate::models::feishu_message::MessageReceiveRequest;
 
 // /// response for connect
 // #[post("/", data = "<request>")]
@@ -13,10 +17,31 @@ use crate::models::feishu_message::MessageReceiveRequest;
 
 /// response for message receive
 #[post("/", data = "<request>")]
-async fn response(request: Json<MessageReceiveRequest>) -> Result<Json<String>, String> {
-    let challenge = request.header.token.clone();
-    print!("challenge: {}", challenge);
-    Ok(Json(String::from("123")))
+async fn response(request: Json<MessageReceiveRequest>) -> Result<(), String> {
+    let sender = request.event.sender.clone();
+
+    let feishu_api = FeishuApi {
+        base_url: env::var("BASE_URL").expect("BASE_URL must be set"),
+        app_id: env::var("APP_ID").expect("APP_ID must be set"),
+        app_secret: env::var("APP_SECRET").expect("APP_SECRET must be set"),
+    };
+
+    let body = SendMessageBody {
+        receive_id: sender.sender_id.open_id,
+        msg_type: "text".to_string(),
+        content: json!({
+            "text": "Hello, world!"
+        })
+        .to_string(),
+        uuid: None,
+    };
+
+    feishu_api
+        .send_message(ReceiveIdType::OpenId, body)
+        .await
+        .expect("send_message failed");
+
+    Ok(())
 }
 
 #[catch(404)]
